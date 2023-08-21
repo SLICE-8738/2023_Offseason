b13afc5ce2522828e5ce505df5aa9ba7cedd19a4
MikeVey42 <steelege07@gmail.com>
2023-08-21 15:24:01 -0400

    set rainbowLEDs to default command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -77,9 +77,9 @@ public class RobotContainer {
     configureButtonBindings();
 
     m_drivetrain.setDefaultCommand(m_swerveDriveClosedLoop);
-
     m_elevator.setDefaultCommand(m_manualElevator);
     m_arm.setDefaultCommand(m_manualArm);
+    m_leds.setDefaultCommand(m_rainbowLEDs);
     
   }
 
